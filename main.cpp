cf45ad186d25340a2b20407b8d8816bee153d143
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-17 11:56:49 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-// write your code here
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	int number;
+	int i;
+	srand(time(0));
+	cin >> number;
+	for (i = 1; i <=15; i++) {
+		number = rand() % 1000;
+		cout << number << "\t";
+		
+	}
+	
+}
