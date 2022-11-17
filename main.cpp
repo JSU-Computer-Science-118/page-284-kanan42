135305118c2ea4124e27f531bdb0f8d605e5fb36
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-17 12:25:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
-#include<stdlib.h>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 int main()
 {
 	int number;
 	int i;
 	srand(time(0));
-	cin >> number;
-	for (i = 1; i <=15; i++) {
+	int limit;
+	cin >> limit;
+	for (i = 1; i <= limit; i++) {
 		number = rand() % 1000;
 		cout << number << "\t";
-		
+
 	}
-	
+
 }
